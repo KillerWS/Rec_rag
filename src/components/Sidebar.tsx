c7092f8583778fe7858f75116337a360af99cedd
chris <495032732@qq.com>
2025-12-09 17:50:13 +0100

    1209

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ const Sidebar = () => {
       <div className="w-64 bg-gray-800 text-white p-4">
         <h2 className="text-xl font-bold">推荐列表</h2>
         <ul>
-          <li className="mt-2">推荐1</li>
+          <li className="mt-2">推荐11</li>
           <li className="mt-2">推荐2</li>
           <li className="mt-2">推荐3</li>
         </ul>
